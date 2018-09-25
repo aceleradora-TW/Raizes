2b800734b1f64cb7ba7768805a23c4186aac5481
othman853 <anuar.yasser@gmail.com>
2018-09-25 14:42:18 -0300

    Atualiza testes funcionais

--- testes-funcionais/cypress/integration/home.test.js
+++ testes-funcionais/cypress/integration/home.test.js
@@ -8,9 +8,10 @@ describe('A pagina Home', () => {
     cy.get('h1').should('contain', 'Esqueleto Ambulante')
   })
 
-  it('tem links para as paginas "inicio" e "participantes"', () => {
+  it('tem links para as paginas "inicio" e "pessoas" e "cadastrar pessoa"', () => {
     cy.get('a > li').eq(0).should('contain', 'Início')
-    cy.get('a > li').eq(1).should('contain', 'Participantes')
+    cy.get('a > li').eq(1).should('contain', 'Pessoas')
+    cy.get('a > li').eq(2).should('contain', 'Cadastrar Pessoa')
   })
 
 })
