ec59a28ca624b32cd487575ccc854abf2fc04ac3
yrachid <anuar.yasser@gmail.com>
2019-05-09 16:42:54 -0300

    09 @yrachid: Busca e insercao de produtos

--- src/main/java/com/thoughtworks/aceleradora/repositorios/ProdutoRepositorioJdbc.java
+++ src/main/java/com/thoughtworks/aceleradora/repositorios/ProdutoRepositorioJdbc.java
@@ -2,10 +2,12 @@ package com.thoughtworks.aceleradora.repositorios;
 
 import com.thoughtworks.aceleradora.dominio.Produto;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ProdutoRepositorioJdbc {
@@ -13,17 +15,37 @@ public class ProdutoRepositorioJdbc {
     private JdbcTemplate template;
 
     private static final String BUSCAR_TODOS = "SELECT id, nome FROM produtos";
-
-    private final RowMapper<Produto> mapeadorDeProduto = (resultadoConsulta, numeroDaLinha) -> new Produto(
-            resultadoConsulta.getLong("id"),
-            resultadoConsulta.getString("nome")
-    );
+    private static final String BUSCAR_POR_ID = "SELECT id, nome FROM produtos WHERE id = ?";
+    private static final String INSERIR = "INSERT INTO produtos (nome) VALUES (?)";
 
     public ProdutoRepositorioJdbc(JdbcTemplate template) {
         this.template = template;
     }
 
     public List<Produto> buscarTodos() {
-        return template.query(BUSCAR_TODOS, mapeadorDeProduto);
+        return template.query(BUSCAR_TODOS, (resultadoConsulta, numeroDaLinha) -> mapearDadosParaObjeto(resultadoConsulta));
+    }
+
+    public Optional<Produto> buscarPorId(Long id) {
+        Object[] parametrosDaConsulta = new Object[]{id};
+
+        return template.query(BUSCAR_POR_ID, parametrosDaConsulta, resultadoDaConsulta -> {
+            if (resultadoDaConsulta.next()) {
+                return Optional.of(mapearDadosParaObjeto(resultadoDaConsulta));
+            }
+
+            return Optional.empty();
+        });
+    }
+
+    public void inserir(Produto produto) {
+        template.update(INSERIR, produto.getNome());
+    }
+
+    private Produto mapearDadosParaObjeto(ResultSet resultadoDaConsulta) throws SQLException {
+        return new Produto(
+                resultadoDaConsulta.getLong("id"),
+                resultadoDaConsulta.getString("nome")
+        );
     }
 }
--- src/test/java/com/thoughtworks/aceleradora/repositorios/ProdutoRepositorioJdbcTest.java
+++ src/test/java/com/thoughtworks/aceleradora/repositorios/ProdutoRepositorioJdbcTest.java
@@ -12,16 +12,19 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
-import static java.util.Arrays.asList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = ProdutoRepositorioJdbcTest.Config.class)
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
-@Sql("/test-produtos-data.sql")
+@Sql(scripts = "/inserir-produtos-de-teste.sql", executionPhase = BEFORE_TEST_METHOD)
+@Sql(scripts = "/limpar-dados-de-teste.sql", executionPhase = AFTER_TEST_METHOD)
 public class ProdutoRepositorioJdbcTest {
 
     @Configuration
@@ -37,11 +40,31 @@ public class ProdutoRepositorioJdbcTest {
 
         List<Produto> resultado = repositorioJdbc.buscarTodos();
 
-        assertThat(resultado, is(asList(
+        assertThat(resultado, is(List.of(
                 new Produto(1L, "Banana"),
                 new Produto(2L, "Maca"),
                 new Produto(3L, "Batata")
         )));
+    }
 
+    @Test
+    public void encontraUmProdutoPorId() {
+        Optional<Produto> batata = repositorioJdbc.buscarPorId(3L);
+
+        assertThat(batata.get(), is(new Produto(3L, "Batata")));
+    }
+
+    @Test
+    public void quandoRegistroNaoExisteRetornaOpcionalVazio() {
+        Optional<Produto> produtoInexistente = repositorioJdbc.buscarPorId(100L);
+
+        assertThat(produtoInexistente.isPresent(), is(false));
+    }
+
+    @Test
+    public void insereUmNovoRegistro() {
+       repositorioJdbc.inserir(new Produto("Banana"));
+
+       assertThat(repositorioJdbc.buscarPorId(4L).get(), is(new Produto(4L, "Banana")));
     }
 }
