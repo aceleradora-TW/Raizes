82ae8f79f0c9370327d3830dc777070b338cf151
yeymono <dudaschuler2011@gmail.com>
2019-05-09 14:29:41 -0300

    04/@yeymono, @Jurley-Ribeiro : Desenvolvendo alert de excluir lista

--- src/main/resources/assets/js/pages/minha-lista.js
+++ src/main/resources/assets/js/pages/minha-lista.js
@@ -1,9 +1,11 @@
 export default {
-    excluirLista: (txt) => {
-        if (confirm(`${txt} \n Você deseja realmente excluir a lista selecionada?`)) {
-            alert("Item excluído com sucesso.");
+    excluirLista: (form) => {
+    
+        if (confirm(`joijoijoij \n Você deseja realmente excluir a lista selecionada?`)) {
+            form.submit();
         } else {
-
+            
         }
     },
+
 }
