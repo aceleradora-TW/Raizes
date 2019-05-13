08f9d38b87769925ab7075a8edfa4478ada041f0
cassia11 <cassiagomes0102@gmail.com>
2019-05-13 10:35:18 -0300

    13/@dfernandos, @gabrielfarias0605:Resolvendo PR altlteramos os dados (Interger para bigint)

--- src/main/resources/db/migration/V3__Create_produtos_table.sql
+++ src/main/resources/db/migration/V3__Create_produtos_table.sql
@@ -2,10 +2,10 @@ CREATE TABLE produtos (
 
   id serial primary key,
   nome varchar(255) not null,
-  id_categorias integer not null,
-  id_tipo_produtos integer not null,
+  id_categorias bigint not null,
+  id_tipo_produtos bigint not null,
 
 
   constraint fk_categorias foreign key (id_categorias) references categorias(id),
-  constraint fk_tipo_produtos foreign key (id_tipo_produtos) references tipo_produtos(id)
+  constraint fk_tipos_produtos foreign key (id_tipo_produtos) references tipos_produtos(id)
 );
--- src/main/resources/db/migration/V5__Create_listas_produtos_table.sql
+++ src/main/resources/db/migration/V5__Create_listas_produtos_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE listas_produtos (
     id serial primary key,
-    id_produto integer not null,
-    id_lista integer not null,
+    id_produto bigint not null,
+    id_lista bigint not null,
 
-    constraint fk_produto foreign key (id_produto) references produtos(id),
-    constraint fk_lista foreign key (id_lista) references listas(id)
+    constraint fk_produtos foreign key (id_produto) references produtos(id),
+    constraint fk_listas foreign key (id_lista) references listas(id)
 
 );
