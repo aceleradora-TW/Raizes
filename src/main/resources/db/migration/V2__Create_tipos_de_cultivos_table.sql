d226a5233355c3d71f196255600b3dce8800de07
cassia11 <cassiagomes0102@gmail.com>
2019-05-13 13:45:32 -0300

    13/@dfernandos, @gabrielfarias0605: Altera nome da tabela tipo produtos para tipos de cultivo

--- src/main/resources/db/migration/V2__Create_tipos_de_cultivos_table.sql
+++ src/main/resources/db/migration/V2__Create_tipos_de_cultivos_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tipos_produtos (
+CREATE TABLE tipos_de_cultivos (
     id serial primary key,
     nome varchar(255) not null
 
--- src/main/resources/db/migration/V3__Create_produtos_table.sql
+++ src/main/resources/db/migration/V3__Create_produtos_table.sql
@@ -3,9 +3,9 @@ CREATE TABLE produtos (
   id serial primary key,
   nome varchar(255) not null,
   id_categorias bigint not null,
-  id_tipo_produtos bigint not null,
+  id_tipos_de_cultivos bigint not null,
 
 
   constraint fk_categorias foreign key (id_categorias) references categorias(id),
-  constraint fk_tipos_produtos foreign key (id_tipo_produtos) references tipos_produtos(id)
+  constraint fk_tipos_de_cultivos foreign key (id_tipos_de_cultivos) references tipos_de_cultivos(id)
 );
