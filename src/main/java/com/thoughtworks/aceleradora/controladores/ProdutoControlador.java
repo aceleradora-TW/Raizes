4be4d1f8b916c87fb9dbbe4a291b630dd5318c14
barbaraanger <barbaraanger@hotmail.com>
2019-05-22 14:52:00 -0300

    49/@daniinyan: Cria ProdutoControlador

---
+++ src/main/java/com/thoughtworks/aceleradora/controladores/ProdutoControlador.java
@@ -0,0 +1,38 @@
+package com.thoughtworks.aceleradora.controladores;
+
+import com.thoughtworks.aceleradora.dominio.Breadcrumb;
+import com.thoughtworks.aceleradora.servicos.CategoriaServico;
+import com.thoughtworks.aceleradora.servicos.ProdutoServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.function.Consumer;
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoControlador {
+
+    private ProdutoServico produtoServico;
+    private CategoriaServico categoriaServico;
+
+    private final Consumer<Breadcrumb> partesComunsDoBreadCrumb = breadcrumb -> breadcrumb
+            .pagina("Início", "/");
+
+    @Autowired
+    public ProdutoControlador(ProdutoServico produtoServico, CategoriaServico categoriaServico) {
+        this.produtoServico = produtoServico;
+        this.categoriaServico = categoriaServico;
+    }
+
+    @GetMapping("/cadastro")
+    public void cadastarProduto(Model modelo, Breadcrumb breadcrumb) {
+            breadcrumb
+                    .aproveitar(partesComunsDoBreadCrumb)
+                    .pagina("Cadastro", "/produtos/cadastro");
+    }
+
+
+}
