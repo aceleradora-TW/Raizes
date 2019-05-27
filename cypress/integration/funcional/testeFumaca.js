70dbe6fc6daad8795790e59255abc1fb656da089
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-05-27 15:55:55 -0300

    47/@franias,@Luiza-Lamarque:Configura para o raizes-homologacao

--- cypress/integration/funcional/testeFumaca.js
+++ cypress/integration/funcional/testeFumaca.js
@@ -1,13 +1,13 @@
 describe('Testa ambiente de produção', function() {
     it('Carrega página do raízes', function() {
-        cy.visit('https://raizes-producao.herokuapp.com/')
+        cy.visit('https://raizes-homologacao.herokuapp.com/')
 
     })
 })
 
 describe('Testa titulo', function() {
     it('Testa titulo da página', function() {
-        cy.visit('https://raizes-producao.herokuapp.com/')
+        cy.visit('https://raizes-homologacao.herokuapp.com/')
         cy.title().should('eq', 'Aceleradora Ágil - Esqueleto Ambulante')
     })
 })
