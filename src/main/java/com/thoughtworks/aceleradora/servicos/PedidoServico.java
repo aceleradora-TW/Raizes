52bf4bc3dd542218a5e293ee45c0cf0eee90b709
brendamoniqueb <bmbrocardo@gmail.com>
2019-05-28 16:03:32 -0300

    34/@brendamoniqueb, @daniinyan: começa metodo pegarProdutoresDosProdutos

--- src/main/java/com/thoughtworks/aceleradora/servicos/PedidoServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/PedidoServico.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.aceleradora.servicos;
+import com.thoughtworks.aceleradora.dominio.MinhaLista;
 import com.thoughtworks.aceleradora.dominio.Pedido;
+import com.thoughtworks.aceleradora.dominio.Produto;
+import com.thoughtworks.aceleradora.dominio.Produtor;
 import com.thoughtworks.aceleradora.repositorios.PedidoRepositorio;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PedidoServico {
@@ -9,6 +14,19 @@ public class PedidoServico {
 
     public PedidoServico(PedidoRepositorio repositorio) {
         this.repositorio = repositorio;
+    }
+
+    public List<Produtor> pegaProdutoresDosProdutos(MinhaLista minhaLista) {
+        //1. percorrer a lista
+        //2. pegar todos os produtores de um produto
+        //3. armazenar produtores em nova lista
+        //4. retornar lista de produtores encontrados
+
+        for (Produto produto : minhaLista.getProdutos()) {
+
+        }
+
+        return null;
     }
 
 }
