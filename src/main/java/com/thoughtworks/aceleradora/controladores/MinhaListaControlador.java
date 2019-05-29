1029ee5e64dc8691a6307e26f93bb360a6685ae5
cassia11 <cassiagomes0102@gmail.com>
2019-05-29 10:53:09 -0300

    69/@cassia, @ingridDuarte:Cria campo para o nome da lista

--- src/main/java/com/thoughtworks/aceleradora/controladores/MinhaListaControlador.java
+++ src/main/java/com/thoughtworks/aceleradora/controladores/MinhaListaControlador.java
@@ -10,9 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Consumer;
 
 @Controller
---
+++ src/main/java/com/thoughtworks/aceleradora/controladores/PedidoControlador.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.aceleradora.controladores;
+
+import com.thoughtworks.aceleradora.dominio.Breadcrumb;
+import com.thoughtworks.aceleradora.dominio.MinhaLista;
+import com.thoughtworks.aceleradora.servicos.MinhaListaServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.function.Consumer;
+
+@Controller
+@RequestMapping("/pedidos")
+public class PedidoControlador {
+
+    private MinhaListaServico minhaListaServico;
+
+    private final Consumer<Breadcrumb> partesComunsDoBreadCrumb = breadcrumb -> breadcrumb
+            .pagina("Início", "/");
+
+    @Autowired
+    public PedidoControlador(MinhaListaServico minhaListaServico) {
+        this.minhaListaServico = minhaListaServico;
+    }
+
+
+    @GetMapping("/{id}/visualizar")
+    public String visualizatPedido( @PathVariable("id") Long id, Model modelo, Breadcrumb breadcrumb) {
+        MinhaLista lista = minhaListaServico.encontraUm(id);
+        breadcrumb
+                .aproveitar(partesComunsDoBreadCrumb)
+                .pagina("Pedidos", "/pedidos")
+                .pagina("Visualizar pedido", "/pedidos/visualizar");
+
+        modelo.addAttribute("lista", lista);
+        return "pedido/visualizar-pedido";
+    }
+
+    @PostMapping("/{id}/visualizar")
+    public String visualizarPedido(MinhaLista minhaLista, @PathVariable("id") Long id, Breadcrumb breadcrumb) {
+        MinhaLista lista = minhaListaServico.encontraUm(id);
+        breadcrumb
+                .aproveitar(partesComunsDoBreadCrumb)
+                .pagina("Pedidos", "/pedidos")
+                .pagina("Visualizar pedido", "/pedidos/visualizar");
+
+        lista.setNome(minhaLista.getNome());
+        return "pedido/visualizar-pedido";
+    }
+}
