26f697d440df518f41c94065c5355fbd1cdd6535
daniinyan <daniela.dbass@gmail.com>
2019-06-03 13:53:23 -0300

    66/@dfernandos, @daniinyan: Cria e relaciona classe Cidade

---
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Cidade.java
@@ -0,0 +1,47 @@
+package com.thoughtworks.aceleradora.dominio;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity(name = "cidades")
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_estados" )
+    private Estado estado;
+
+    public Cidade(String nome, Estado estado) {
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
--- src/main/java/com/thoughtworks/aceleradora/dominio/Estado.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Estado.java
@@ -1,12 +1,12 @@
 package com.thoughtworks.aceleradora.dominio;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
-@Entity
+@Entity(name = "estados")
 public class Estado {
 
     @Id
@@ -14,6 +14,10 @@ public class Estado {
     private Long id;
 
     private String uf;
+
+    @OneToMany(mappedBy = "estado")
+    @OrderBy(value="nome")
+    private List<Cidade> cidades;
 
     public Estado(String uf) {
         this.uf = uf;
@@ -33,5 +37,13 @@ public class Estado {
 
     public void setUf(String uf) {
         this.uf = uf;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
     }
 }
