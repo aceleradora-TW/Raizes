9f6ab3123920f6fafa9263b1b8e2c24c2ba7876f
barbaraanger <barbaraanger@hotmail.com>
2019-06-12 11:24:30 -0300

    94/@barbaraanger,@yeymono: adiciona import que falta

--- src/main/java/com/thoughtworks/aceleradora/servicos/MinhaListaServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/MinhaListaServico.java
@@ -6,6 +6,7 @@ import com.thoughtworks.aceleradora.repositorios.MinhaListaRepositorio;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MinhaListaServico {
