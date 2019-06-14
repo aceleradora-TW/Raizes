74e95297377c199b8141e1f84f37c69d9c95abb7
ghidorsi <ghidorsi@thoughtworks.com>
2019-06-14 14:30:41 -0300

    @ghidorsi: Exemplo cadastro ProdutoProdutor

--- src/main/java/com/thoughtworks/aceleradora/servicos/ProdutorServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/ProdutorServico.java
@@ -2,11 +2,17 @@ package com.thoughtworks.aceleradora.servicos;
 
 import com.thoughtworks.aceleradora.dominio.Produtor;
 import com.thoughtworks.aceleradora.repositorios.ProdutorRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutorServico {
     private ProdutorRepositorio repositorio;
+
+    @Autowired
+    public ProdutorServico(ProdutorRepositorio repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public Produtor encontraUm(Long id){
         return repositorio.findById(id).get();
