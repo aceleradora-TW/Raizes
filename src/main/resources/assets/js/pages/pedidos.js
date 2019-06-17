3b866eb4350633cb91e04f9e90c655d10cdf4a38
JulianeMartins <jumartinsvargas@gmail.com>
2019-06-17 14:34:31 -0300

    adiciona modal de exclusão de pedido

--- src/main/resources/assets/js/pages/pedidos.js
+++ src/main/resources/assets/js/pages/pedidos.js
@@ -1,4 +1,12 @@
 export default {
-    exibeConfirmacaoDeExclusaoDoPedido: () => confirm("Deseja excluir a compra selecionada?")
-}
+    exibeConfirmacaoDeExclusaoDoPedido: (event) => {
+        let form = event.target;
 
+        window.Modal
+        .warning()
+        .okAction("Sim", () => { form.submit();})
+        .cancelAction("Não")
+        .show('Você tem certeza que deseja excluir esta compra?');
+        return false;
+    }
+}
