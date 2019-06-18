c812fe91e40bda9bb692ff2911908f58218b88f1
barbaraanger <barbaraanger@hotmail.com>
2019-06-18 15:57:06 -0300

    121/@barbaraanger: valida formulario de editar lista do estoque

--- src/main/resources/assets/js/pages/editaProdutoEstoque.js
+++ src/main/resources/assets/js/pages/editaProdutoEstoque.js
@@ -1,12 +1,6 @@
 import Validator from '../utils/validator'
 
 export default {
-  exibeConfirmacaoDeCancelamento: (href) => Modal
-    .warning()
-    .okAction("Sim", () => { window.location.href = href })
-    .cancelAction("Não")
-    .show("Realmente deseja cancelar a edição? Os dados não serão salvos."),
-
     validaFormulario: () => {
       const validador = new Validator('#formEditarProduto');
      
