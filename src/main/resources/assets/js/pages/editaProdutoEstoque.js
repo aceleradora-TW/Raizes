8b460a1dda94133618337b625ee1f0329986dc0c
yeymono <dudaschuler2011@gmail.com>
2019-06-19 15:56:57 -0300

    121/@mjoliveira, @yeymono: apresenta unidade de medida na tela de edição de produto

--- src/main/resources/assets/js/pages/editaProdutoEstoque.js
+++ src/main/resources/assets/js/pages/editaProdutoEstoque.js
@@ -3,7 +3,8 @@ import Validator from '../utils/validator'
 export default {
     validaFormulario: () => {
       const validador = new Validator('#formEditarProduto');
-     
+      validador.setRequiredValidatorToFieldGroup('preco', 'Campo não pode ser vazio e não pode ter números negativos');
+
       validador.setRulesForEachInputs({
         rules: {
           required: true
