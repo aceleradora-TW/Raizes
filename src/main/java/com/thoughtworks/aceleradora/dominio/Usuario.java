208904b8de75dec980d1cff9f3c2bf28ac494e62
brendamoniqueb <bmbrocardo@gmail.com>
2019-06-19 13:20:30 -0300

    113/@jurley-ribeiro, @brendamoniqueb: Atualiza as classes usuario, cliente e produtor.

--- src/main/java/com/thoughtworks/aceleradora/dominio/Usuario.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/Usuario.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.aceleradora.dominio;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
@@ -9,78 +12,36 @@ import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
-@Entity(name = "usuarios")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-public class Usuario {
+@Data
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "tipo_de_usuario")
+@Entity
+@Table(name = "usuarios")
+
+abstract class Usuario {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
-    @Column(unique = true)
-    private String email;
-
     private String senha;
+    private String nome;
+    private String contato;
+
+    @ManyToOne
+    @JoinColumn(name = "id_endereco")
+    private Endereco endereco;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_de_usuario")
+    @Column(name = "tipo_de_usuario", insertable = false, updatable = false)
     private TipoDeUsuario tipoDeUsuario;
 
-    @OneToOne(mappedBy = "usuario")
-    private Cliente cliente;
-
-    @OneToOne(mappedBy = "usuario")
-    private Produtor produtor;
-
-    public Usuario() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
+    public Usuario(String email, String senha, String nome, String contato, Endereco endereco) {
         this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
         this.senha = senha;
-    }
-
-    public TipoDeUsuario getTipoDeUsuario() {
-        return tipoDeUsuario;
-    }
-
-    public void setTipoDeUsuario(TipoDeUsuario tipoDeUsuario) {
-        this.tipoDeUsuario = tipoDeUsuario;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Produtor getProdutor() {
-        return produtor;
-    }
-
-    public void setProdutor(Produtor produtor) {
-        this.produtor = produtor;
+        this.nome = nome;
+        this.contato = contato;
+        this.endereco = endereco;
     }
 }
 
