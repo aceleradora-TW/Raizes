6fc179cd77fd90d5a36f813469dbde60000b6c45
cassia11 <cassiagomes0102@gmail.com>
2019-06-21 14:49:32 -0300

    104/Cria exceção para metodo que procura um produtor

--- src/main/java/com/thoughtworks/aceleradora/servicos/ProdutorServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/ProdutorServico.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.aceleradora.servicos;
 
 import com.thoughtworks.aceleradora.dominio.Produtor;
+import com.thoughtworks.aceleradora.dominio.excecoes.ProdutorNaoEncontradoExcecao;
 import com.thoughtworks.aceleradora.repositorios.ProdutorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ public class ProdutorServico {
     }
 
     public Produtor encontraUm(Long id){
-        return repositorio.findById(id).get();
+        return repositorio
+                .findById(id)
+                .orElseThrow(ProdutorNaoEncontradoExcecao::new);
     }
 }
