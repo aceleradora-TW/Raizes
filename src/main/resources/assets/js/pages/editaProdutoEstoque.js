2ff0fde5b467efc3d1c82c2ce0f67bba2f8946f7
yeymono <dudaschuler2011@gmail.com>
2019-06-21 09:56:58 -0300

    121/@mjoliveira,@yeymono: adiciona mensagem correta de validação de js

--- src/main/resources/assets/js/pages/editaProdutoEstoque.js
+++ src/main/resources/assets/js/pages/editaProdutoEstoque.js
@@ -4,7 +4,8 @@ export default {
     validaFormulario: () => {
       const validador = new Validator('#formEditarProduto');
       validador.setRequiredValidatorToFieldGroup('preco', 'Campo não pode ser vazio e não pode ter números negativos');
-
+      validador.setRequiredValidatorToFieldGroup('quantidadeEstoque','Campo não pode ser vazio e não pode ter números negativos');
+      
       validador.setRulesForEachInputs({
         rules: {
           required: true
