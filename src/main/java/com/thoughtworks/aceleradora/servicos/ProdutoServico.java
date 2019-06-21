5460fecf7635bc091cd067c3574b74f45d7977aa
cassia11 <cassiagomes0102@gmail.com>
2019-06-21 14:57:58 -0300

    104/Remove métodos que não estavam sendo utilizados

--- src/main/java/com/thoughtworks/aceleradora/servicos/ProdutoServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/ProdutoServico.java
@@ -24,14 +24,5 @@ public class ProdutoServico {
     public Produto salvar (Produto produto) {
         return repositorio.save(produto);
     }
-
-    public Produto encontraUm(Long id) {
-        return repositorio.findById(id).get();
-    }
-
-    public Optional<Produto> encontraUmNome(String nome) { return repositorio.findByNome(nome); }
-
-    public String encontraUnidadeMedida (Long id){
-        return repositorio.findById(id).get().getUnidadeMedida().getCodigo();
-    }
+    
 }
