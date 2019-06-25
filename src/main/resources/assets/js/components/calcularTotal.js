ca59f9f8277c15bc8ecb39980e21bf5813d5312b
franias <franciellipdias@gmail.com>
2019-06-25 15:10:12 -0300

    teste/@franias: Termina a refatoracao calculando todos os cards

--- src/main/resources/assets/js/components/calcularTotal.js
+++ src/main/resources/assets/js/components/calcularTotal.js
@@ -2,32 +2,15 @@ function duasCasasAposVirgula(num){
     return parseFloat(Math.round(num * Math.pow(10, 2)) /Math.pow(10,2)).toFixed(2);
 }
 const fn = (event) => {
-    console.log(event);
 
     const inputQuantidade = event.target;
     const totalProduto = $(inputQuantidade).parents('#totalProduto');
-
-    console.log(totalProduto);
 
     const preco = totalProduto.find('#preco').text();
     
     const quantidade = event.target.value || 0;
     const resultado = preco * quantidade;
-    $('#resultado').text(duasCasasAposVirgula(resultado)); 
+    totalProduto.find('#resultado').text(duasCasasAposVirgula(resultado)); 
     
 }
-export default fn;
-
-
-// const fn = (event) => {
-//     console.log(event);
-    
-//     const inputQuantidade = event.target;
-//     const quantidade = inputQuantidade.value() || 0;
-//     const totalProduto = $(inputQuantidade).parents('.totalProduto');
-//     var preco = totalProduto.find('#preco').text();
-    
-//     var resultado = preco * quantidade;
-//     $('.resultado').text(duasCasasAposVirgula(resultado,2)); 
-    
-// }
+export default fn; 
