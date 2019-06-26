a65a42e18366a5ac31304152204e63e1d65051f9
brendamoniqueb <bmbrocardo@gmail.com>
2019-06-26 16:05:59 -0300

    expedite/@brendamoniqueb, @gabrielfarias0605:Validações JavaScript

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -14,6 +14,7 @@ import Burger from './components/menu-burger'
 import EditarPedido from './pages/editaPedido'
 import Produtos from './pages/produtos'
 import CadastraProduto from './pages/cadastraProduto'
+import validaCadastraProduto from './pages/validaCadastraProduto'
 
 
 
@@ -21,6 +22,8 @@ $(function () {
     CriarLista.validaFormulario();   
     EditarLista.validaFormulario();
     CadastraProduto.atualizaUnidadeMedida();
+    validaCadastraProduto.validaFormulario();
+
 
     window.MinhasListas = MinhasListas;
     window.EditarLista = EditarLista;
