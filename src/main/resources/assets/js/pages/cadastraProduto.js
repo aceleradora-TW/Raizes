8a08beab8e85e75a146c174b553a5fa3c10dc769
cassia11 <cassiagomes0102@gmail.com>
2019-06-28 14:16:26 -0300

    expedite/@franias: nao consegue refatorar o commit que fizemos a refatoracao

--- src/main/resources/assets/js/pages/cadastraProduto.js
+++ src/main/resources/assets/js/pages/cadastraProduto.js
@@ -1,17 +1,17 @@
 export default {
-  atualizaUnidadeMedida: (e) => {
-    const seletorDeProdutos = $('#seletorDeProdutos');
+  // atualizaUnidadeMedida: (e) => {
+  //   const seletorDeProdutos = $('#seletorDeProdutos');
     
-    if(seletorDeProdutos.get().length < 1) {
-      return;
-    }
+  //   if(seletorDeProdutos.get().length < 1) {
+  //     return;
+  //   }
 
-    const opcaoSelecionada = seletorDeProdutos.find('option:selected');
-    const unidadeMedidaElement = $('#unidadeMedida');
-    const unidadeMedidaElement2 = $('#unidadeMedida2');
-    const unidadeMedida = opcaoSelecionada.attr('data-unidade-medida'); 
+  //   const opcaoSelecionada = seletorDeProdutos.find('option:selected');
+  //   const unidadeMedidaElement = $('#unidadeMedida');
+  //   const unidadeMedidaElement2 = $('#unidadeMedida2');
+  //   const unidadeMedida = opcaoSelecionada.attr('data-unidade-medida'); 
 
-    unidadeMedidaElement.text(unidadeMedida);
-    unidadeMedidaElement2.text(unidadeMedida);
-  }
+  //   unidadeMedidaElement.text(unidadeMedida);
+  //   unidadeMedidaElement2.text(unidadeMedida);
+  // }
 }
--- src/main/resources/assets/js/pages/produtos.js
+++ src/main/resources/assets/js/pages/produtos.js
@@ -10,13 +10,13 @@ export default {
         
         if(seletorDeProdutos.get().length < 1) {
           return;
-        }
+        };
     
-        const opcaoSelecionada = seletorDeProdutos.find('option:selected');
-        const unidadeMedidaElement = $('#unidadeMedida');
-        const unidadeMedida = opcaoSelecionada.attr('data-unidade-medida'); 
-    
-        unidadeMedidaElement.text(unidadeMedida);
+        const textoUnidadeMedida = seletorDeProdutos.find('option:selected');
+        .querySelectorAll('.unidade-medida')
+        .forEach(unidadeMedidaElement => {
+          unidadeMedidaElement.text(textoUnidadeMedida)
+      });
     }
 
 }
