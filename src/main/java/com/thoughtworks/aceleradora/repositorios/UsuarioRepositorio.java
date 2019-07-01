5ce5e240e48f581d6089467d7880e51c0a4c05a9
barbaraanger <barbaraanger@hotmail.com>
2019-07-01 15:10:23 -0300

    110/@brendamoniqueb, @dfernandos: Retira métodos não utilizados do UsuarioRepositorio

--- src/main/java/com/thoughtworks/aceleradora/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/thoughtworks/aceleradora/repositorios/UsuarioRepositorio.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.aceleradora.repositorios;
 
-public interface UsuarioRepositorio {
+import com.thoughtworks.aceleradora.dominio.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends CrudRepository<Usuario, Long> {
+        Usuario findByEmail(String email);
 }
+
