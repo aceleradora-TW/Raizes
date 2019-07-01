6b9c11fae0b3cfa11ffe8eb2a63745bdef9bcc43
barbaraanger <barbaraanger@hotmail.com>
2019-07-01 15:11:17 -0300

    110/@brendamoniqueb, @dfernandos: Cria métodos salvaCliente e salvaProdutor

--- src/main/java/com/thoughtworks/aceleradora/servicos/RegistrarServico.java
+++ src/main/java/com/thoughtworks/aceleradora/servicos/RegistrarServico.java
@@ -1,27 +1,37 @@
 package com.thoughtworks.aceleradora.servicos;
 
 import com.thoughtworks.aceleradora.dominio.Cliente;
+import com.thoughtworks.aceleradora.dominio.Produtor;
 import com.thoughtworks.aceleradora.repositorios.ClienteRepositorio;
+import com.thoughtworks.aceleradora.repositorios.ProdutorRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UsuarioServico {
+public class RegistrarServico {
 
     private ClienteRepositorio clienteRepositorio;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private ProdutorRepositorio produtorRepositorio;
 
     @Autowired
-    public UsuarioServico(ClienteRepositorio clienteRepositorio, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.usuarioRepositorio = usuarioRepositorio;
+    public RegistrarServico(ClienteRepositorio clienteRepositorio, BCryptPasswordEncoder bCryptPasswordEncoder, ProdutorRepositorio produtorRepositorio) {
+        this.clienteRepositorio = clienteRepositorio;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.produtorRepositorio = produtorRepositorio;
     }
 
     public void salvarCliente(Cliente cliente) {
         cliente.setSenha(bCryptPasswordEncoder.encode(cliente.getSenha()));
-        .save(cliente);
+        Cliente lixo = clienteRepositorio.save(cliente);
+
+        System.out.println(">>>>>>>>>>> RESULTADO SAVE " + lixo);
     }
 
+    public void salvarProdutor(Produtor produtor) {
+        produtor.setSenha(bCryptPasswordEncoder.encode(produtor.getSenha()));
+        produtorRepositorio.save(produtor);
+    }
 
 }
