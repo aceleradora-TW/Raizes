6698191fd5643488738169e51c320445b13cf4eb
Luiza-Lamarque <luiza-lamarque@hotmail.com>
2019-07-05 15:33:01 -0300

    expedite/validar/@mjoliveira,@Luiza-Lamarque:otimiza chamada de import da mascara

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,5 +1,5 @@
 window.$ = window.jQuery = require('jquery')
-import '../../../../../node_modules/jquery-mask-plugin/dist/jquery.mask';
+import 'jquery-mask-plugin/dist/jquery.mask';
 import '@fortawesome/fontawesome-free/js/all';
 import 'animate.css'
 import '../scss/styles.scss'
