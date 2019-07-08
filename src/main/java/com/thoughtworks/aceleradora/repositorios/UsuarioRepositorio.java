3c5c8a02d71edca68ecc8443d715a8bd1ce3a44d
cassia11 <cassiagomes0102@gmail.com>
2019-07-08 14:02:24 -0300

    expedite/@brendamoniqueb, @cassia11:Mostra mensagem de email ou senha incorreto na tela de login

--- src/main/java/com/thoughtworks/aceleradora/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/thoughtworks/aceleradora/repositorios/UsuarioRepositorio.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends CrudRepository<Usuario, Long> {
-
         Usuario findByEmail(String email);
 }
 
