df660365d1c80f4ddf5e0d7abaec1e97bfb8075a
barbaraanger <barbaraanger@hotmail.com>
2019-07-09 13:28:44 -0300

    192/@barbaraanger,@dfernandos: identa websecurity

--- src/main/java/com/thoughtworks/aceleradora/config/WebSecurityConfig.java
+++ src/main/java/com/thoughtworks/aceleradora/config/WebSecurityConfig.java
@@ -38,25 +38,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/js/**", "/css/**", "/images/**", "/assets/**", "/registrar/**", "/seleciona-tipo-usuario/**")
-                .permitAll()
-                .antMatchers("/produtor").hasAuthority(TipoDeUsuario.PRODUTOR.getValor())
-                .antMatchers("/cliente").hasAuthority(TipoDeUsuario.CLIENTE.getValor())
-                .anyRequest()
-                .authenticated()
+                    .antMatchers("/", "/js/**", "/css/**", "/images/**", "/assets/**", "/registrar/**", "/seleciona-tipo-usuario/**")
+                    .permitAll()
+                    .antMatchers("/produtor").hasAuthority(TipoDeUsuario.PRODUTOR.getValor())
+                    .antMatchers("/cliente").hasAuthority(TipoDeUsuario.CLIENTE.getValor())
+                    .anyRequest()
+                    .authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .usernameParameter("email")
-                .passwordParameter("senha")
-                .defaultSuccessUrl("/")
+                    .loginPage("/login")
+                    .permitAll()
+                    .usernameParameter("email")
+                    .passwordParameter("senha")
+                    .defaultSuccessUrl("/")
                 .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .clearAuthentication(true)
-                .permitAll();
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .clearAuthentication(true)
+                    .permitAll();
         }
 
         @Autowired
