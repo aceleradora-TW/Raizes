53c0a65d4c00eb3b398f941ab32db957e7fef653
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-07-16 11:41:38 -0300

    203/@ingridduarte, @jurley-ribeiro: Envia e-mail personalizado para cada produtor do pedido.

--- src/main/java/com/thoughtworks/aceleradora/dominio/componentes/EmailComponente.java
+++ src/main/java/com/thoughtworks/aceleradora/dominio/componentes/EmailComponente.java
@@ -5,6 +5,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,73 +16,46 @@ import java.util.stream.Collectors;
 public class EmailComponente {
 
     private JavaMailSender mailSender;
-    private String email;
 
     public EmailComponente(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void notificaProdutor(Pedido pedido) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
 
-        List<String> emailProdutores = new ArrayList<>();
+        for (PedidoProdutoProdutor pedidoProdutoProdutor : pedido.getPedidosProdutosProdutores()) {
+            SimpleMailMessage mensagem = new SimpleMailMessage();
 
-        for (PedidoProdutoProdutor p: pedido.getPedidosProdutosProdutores()) {
-            emailProdutores.add(p.getProdutoProdutor().getProdutor().getEmail());
-        }
+            String emailProdutor = pedidoProdutoProdutor.getProdutoProdutor().getProdutor().getEmail();
 
-        email = pedido.getCliente().getEmail();
+            mensagem.setSubject("Raízes - Novo Pedido recebido!");
 
-        mensagem.setSubject("Novo Pedido recebido!");
+            mensagem.setText("Novo pedido recebido!\n" +
+                    "\n" +
+                    "- Nome: " + pedidoProdutoProdutor.getProdutoProdutor().getProdutor().getNome() + "\n" +
+                    "\n ========================== \n" + "\n" +
+                    " Dados da solicitante: " + "\n" +
+                    "- Nome: " + pedido.getCliente().getNome() + "\n" +
+                    "- E-mail: " + pedido.getCliente().getEmail() + "\n" +
+                    "- Telefone: " + pedido.getCliente().getContato() + "\n" +
+                    "\n ========================== \n" + "\n" +
+                    " Dados do pedido: " + "\n" +
+                    "- Nome: " +  "\n" +
+                    "- E-mail: " + "\n" +
+                    "- Telefone: " + "\n"
 
-        mensagem.setText("Novo pedido recebido!\n" + "\n"
-                +
-                "- Raízes: " + "\n" +
-                "\n ========================== \n" + "\n" +
-                " Dados da solicitante: " + "\n" +
-                "- Nome: " + pedido.getCliente().getNome() + "\n" +
-                "- E-mail: " + email + "\n" +
-                "- Telefone: " + pedido.getCliente().getContato() + "\n"
-        );
+            );
 
-        mensagem.setFrom("raizes.agil@gmail.com");
-        mensagem.setTo(pedido
-                .getPedidosProdutosProdutores()
-                .stream()
-                .map(pedidoProdutoProdutor -> pedidoProdutoProdutor.getProdutoProdutor().getProdutor().getEmail())
-                .collect(Collectors.joining(",")));
+            mensagem.setFrom("raizes.agil@gmail.com");
+            mensagem.setTo(emailProdutor);
 
-        try {
-            mailSender.send(mensagem);
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                mailSender.send(mensagem);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
-
-
-
-
-
-
-
-//    public void notificaCliente(PedidoProdutoProdutor pedido) {
-//        SimpleMailMessage mensagem = new SimpleMailMessage();
-
-//            mensagem.setSubject("Status de uma solicitação foi alterado!");
-//
-//            mensagem.setText("Status de uma solicitação de: "+ solicitacao.getNome() +
-//                    " foi alterado para: " + solicitacao.getStatus() + "!");
-//
-//            mensagem.setFrom(this.email);
-//            mensagem.setTo(solicitacao.getEmail());
-//
-//            try {
-//                mailSender.send(mensagem);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
 
