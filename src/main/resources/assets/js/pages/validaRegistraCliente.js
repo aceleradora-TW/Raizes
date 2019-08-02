930f7e364be8a098678d35b53f71030bb6461797
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-08-02 10:15:58 -0300

    expedite/ @todos: Valida os campos do registrar cliente

--- src/main/resources/assets/js/pages/validaRegistraCliente.js
+++ src/main/resources/assets/js/pages/validaRegistraCliente.js
@@ -4,15 +4,15 @@ export default {
   validacaoFormRegistroCliente: () => {
     const validador = new Validator('#registraCliente');
 
-    validador.setRulesForEachInputs({
+    validador.setFieldRule('.validados', {
       rules: {
         required: true
       }
     });
   },
   exibeConfirmacaoDeCancelamento: (href) => Modal
-  .warning()
-  .okAction("Sim", () => { window.location.href =  href })
-  .cancelAction("Não")
-  .show("Realmente deseja sair sem salvar o registro? Os dados não serão salvos.")
+    .warning()
+    .okAction("Sim", () => { window.location.href = href })
+    .cancelAction("Não")
+    .show("Realmente deseja sair sem salvar o registro? Os dados não serão salvos.")
 }
